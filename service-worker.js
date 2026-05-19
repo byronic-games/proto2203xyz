571ab20fc57102a2431fc93746451c1d583ae0e3
byronic-games <byronicman@gmail.com>
2026-05-19 13:50:18 +0100

    Forcing portrait

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_VERSION = "20260519d";
+const CACHE_VERSION = "20260519f";
 const CACHE_NAME = `byronic-52-${CACHE_VERSION}`;
-const GAME_ASSET_VERSION = "20260519d";
+const GAME_ASSET_VERSION = "20260519f";
 
 const APP_SHELL = [
   "./",
