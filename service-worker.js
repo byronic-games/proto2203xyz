5314ab7b5675a226652fea00c6ba239bb0114f0f
byronic-games <byronicman@gmail.com>
2026-05-19 07:18:02 +0100

    Black deck improvements

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_VERSION = "20260518y";
+const CACHE_VERSION = "20260519a";
 const CACHE_NAME = `byronic-52-${CACHE_VERSION}`;
-const GAME_ASSET_VERSION = "20260518y";
+const GAME_ASSET_VERSION = "20260519a";
 
 const APP_SHELL = [
   "./",
